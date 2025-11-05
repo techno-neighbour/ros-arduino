5e80cc293dd8713bd1b7fb88ba27069e2a4b0920
Fudail_Linux <geniusmf8@gmail.com>
2025-11-05 13:31:06 +0530

    Fix the delay between motor and led

--- ros2_ws/src/py_files/py_files/pyserial/car/mot_sen_pub.py
+++ ros2_ws/src/py_files/py_files/pyserial/car/mot_sen_pub.py
@@ -1,5 +1,4 @@
 import rclpy
-import time
 import serial
 import threading
 from rclpy.node import Node
@@ -11,6 +10,7 @@ class MS(Node):
         super().__init__("motor_sensor_pub")
         self.pub = self.create_publisher(MotorSensor, "mot_sen", 10)
         self.sub = self.create_subscription(String, "sen_say", self.listener_callback, 10)
+        self.lock = threading.Lock()
         self.ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
         self.get_logger().info("Starting node...")
         self.timer = self.create_timer(0.05, self.timer_callback)
@@ -26,7 +26,8 @@ class MS(Node):
         while True:
             state = input("Enter direction (1=fwd, 2=back, 3=left, 4=right, 0=stop): ").strip()
             if state in ["0", "1", "2", "3", "4"]:
-                self.last_dire = state
+                with self.lock:
+                    self.last_dire = state
                 self.ser.write(state.encode())
 
     def timer_callback(self):
@@ -39,18 +40,27 @@ class MS(Node):
         elif line:
             self.last_dire = line  # direction
 
-        msg.direction = self.last_dire
-        msg.distance = self.last_dist
+        with self.lock:
+            msg.direction = self.last_dire
+            msg.distance = self.last_dist
 
         self.pub.publish(msg)
 
-    def listener_callback(self, msg):
-        if msg.data:
+    def listener_callback(self, msg):# called only when sensor says 'too close.
+        # to only react if we’re not already backing up
+        if not hasattr(self, 'is_backing_up') or not self.is_backing_up:
             self.get_logger().info(f"Sensor says to {msg.data}")
-            self.ser.write(b'2')
-            time.sleep(0.2)
-            self.ser.write(b'0')
-            msg.data = ""
+            self.ser.write(b'2')  # reverse
+            self.is_backing_up = True
+
+            # using a once timer to stop after 0.3s
+            def stop_once():
+                self.ser.write(b'0')
+                self.is_backing_up = False
+                self.stop_timer.cancel()  # stop this timer
+
+            self.stop_timer = self.create_timer(0.2, stop_once)
+
         
 def main(args=None):
     rclpy.init(args=args)
